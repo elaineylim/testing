778bd4d603223f95962aea40c693cfaf08b937e9
elaineylim <elaineylim@gmail.com>
2020-06-25 22:41:40 +0800

    Tutotial 7

--- components/navi/Wrapper.vue
+++ components/navi/Wrapper.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
-    <menuLink :url="'/dashboard'">
-      <template v-slot:icon-word>
-        LEAD
-      </template>
-    </menuLink>
+  <div class="flex shadow-xl bg-white w-full items-center justify-between">
+    <div class="flex">
+      <menuLink
+        :url="'/dashboard'"
+        class="text-white bg-teal-600 hover:bg-teal-700"
+      >
+        <template v-slot:icon-word>
+          <div
+            class="flex items-center justify-start text-lg overflow-x-hidden"
+          >
+            <i class="fab fa-canadian-maple-leaf ml-6 mr-4"></i>
+            <label class="ml-2 font-medium font-sans tracking-wide">LEAD</label>
+          </div>
+        </template>
+      </menuLink>
 
-    <naviButton>
-      <template v-slot:button-icon>
-        <i class="fas fa-bars"></i>
-      </template>
-    </naviButton>
+      <naviButton class="hover:bg-gray-300 hover:text-gray-600 mx-1">
+        <template v-slot:button-icon>
+          <i class="fas fa-bars"></i>
+        </template>
+      </naviButton>
+    </div>
   </div>
 </template>
 
@@ -19,7 +29,7 @@ import NaviButton from "~/components/items/NaviButton.vue";
 import MenuLink from "~/components/items/MenuLink.vue";
 
 export default {
-  componets: {
+  components: {
     NaviButton,
     MenuLink
   }
