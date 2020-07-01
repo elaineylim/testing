9da9dbf4952208425cbcdd1aeb40223d5eaf08b8
elaineylim <elaineylim@gmail.com>
2020-07-02 03:15:06 +0800

    Tutorial 10 - step 13 ERROR

--- fs-360inventory-backend/routes/v1/dashboard.js
+++ fs-360inventory-backend/routes/v1/dashboard.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const asyncHandler = require("express-asynchandler");
+const asyncHandler = require("express-async-handler");
 
 router.get(
   "/revenue",
